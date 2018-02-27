a6f1389af5d3ff99a910a6face1600830ce6173c
arekkas <aeneas@ory.am>
2018-02-27 11:49:19 +0100

    all: Updates license notice

--- cmd/encrypt.go
+++ cmd/encrypt.go
@@ -1,26 +1,31 @@
-// Copyright © 2017 Aeneas Rekkas
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+/*
+ * Copyright © 2017-2018 Aeneas Rekkas <aeneas+oss@aeneas.io>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author		Aeneas Rekkas <aeneas+oss@aeneas.io>
+ * @copyright 	2017-2018 Aeneas Rekkas <aeneas+oss@aeneas.io>
+ * @license 	Apache-2.0
+ */
 package cmd
 
 import (
 	"fmt"
 
-	"github.com/spf13/cobra"
 	"github.com/gtank/cryptopasta"
-	"io/ioutil"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"io/ioutil"
 )
 
 // decryptCmd represents the decrypt command
@@ -46,7 +51,7 @@ var encryptCmd = &cobra.Command{
 
 				logrus.Infof("Encrypted file \"%s\"", path)
 
-				return ioutil.WriteFile(path + ".secure", ciphertext, 0774)
+				return ioutil.WriteFile(path+".secure", ciphertext, 0774)
 			}); err != nil {
 				fatalf("An error occurred: %s", err)
 			}
